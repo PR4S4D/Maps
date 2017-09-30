6e9f55a12d9c7f1aab54dfe47428085af575f4fd
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-09-30 21:29:20 +0530

    MapsFragment: move camera to given postion in java

--- app/src/main/java/com/slp/maps/MainActivity.java
+++ app/src/main/java/com/slp/maps/MainActivity.java
@@ -2,12 +2,58 @@ package com.slp.maps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady = true;
+        this.googleMap = googleMap;
+        LatLng blore = new LatLng(13.0479029, 77.5103334);
+        CameraPosition target = CameraPosition.fromLatLngZoom(blore, 17);
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        googleMap.addMarker(new MarkerOptions().position(blore).title("Namma City"));
+    }
+
+    public void setMapTypeToSatellite(View view) {
+        if(mapReady){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+    }
+
+    public void setMapTypeToNormal(View view) {
+        if(mapReady){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+    public void setMapTypeToHybrid(View view) {
+        if(mapReady){
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+    }
+
 }
